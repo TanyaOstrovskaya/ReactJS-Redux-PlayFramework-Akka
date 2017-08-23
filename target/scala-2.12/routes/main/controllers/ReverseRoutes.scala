c0fb78bdd066e298679aed9ae4a2e4838e368842
Tatyana Ostrovskaya <ostrtanja@gmail.com>
2017-08-23 09:30:17 +0300

    akka + play sample

---
+++ target/scala-2.12/routes/main/controllers/ReverseRoutes.scala
@@ -0,0 +1,52 @@
+
+// @GENERATOR:play-routes-compiler
+// @SOURCE:C:/Users/user/Desktop/play-test/run/conf/routes
+// @DATE:Wed Aug 23 09:15:47 MSK 2017
+
+import play.api.mvc.Call
+
+
+import _root_.controllers.Assets.Asset
+import _root_.play.libs.F
+
+// @LINE:6
+package controllers {
+
+  // @LINE:6
+  class ReverseHomeController(_prefix: => String) {
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:11
+    def sayHello(name:String): Call = {
+      
+      Call("GET", _prefix + { _defaultPrefix } + "hello/" + play.core.routing.dynamicString(implicitly[play.api.mvc.PathBindable[String]].unbind("name", name)))
+    }
+  
+    // @LINE:6
+    def index(): Call = {
+      
+      Call("GET", _prefix)
+    }
+  
+  }
+
+  // @LINE:9
+  class ReverseAssets(_prefix: => String) {
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:9
+    def versioned(file:Asset): Call = {
+      implicit val _rrc = new play.core.routing.ReverseRouteContext(Map(("path", "/public")))
+      Call("GET", _prefix + { _defaultPrefix } + "assets/" + implicitly[play.api.mvc.PathBindable[Asset]].unbind("file", file))
+    }
+  
+  }
+
+
+}
